19eb7717ce73a6452ec79356fb3b73586f1eec10
yhawin <uwiga_informati2010@yahoo.co.id>
2014-03-23 16:01:13 +0700

    kommit: Adding vaadin

---
+++ src/main/java/org/uwiga/mvc/Servlet01_vaadinUI.java
@@ -0,0 +1,40 @@
+package org.uwiga.mvc;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("servlet01_vaadin")
+public class Servlet01_vaadinUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+/*	@WebServlet(value = "/Servlet01", asyncSupported = true)*/
+	@VaadinServletConfiguration(productionMode = false, ui = Servlet01_vaadinUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
+	}
+
+}
